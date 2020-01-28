8649aceccdc36aa9bd32d0477aa663e532c30f8d
dineshkumar bm <dineshkumar bm@LAPTOP-99V3NSHI>
2020-01-28 12:15:09 +0530

    changes

--- messenger/src/main/java/org/com/dinesh/messenger/MyResource.java
+++ messenger/src/main/java/org/com/dinesh/messenger/MyResource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-/**
+/**hiii rajanna
  * Root resource (exposed at "myresource" path)
  */
 @Path("myresource")
